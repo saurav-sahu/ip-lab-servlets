66e1ff908d5c0e870316b14eb4aec2de060c7bd3
Saurav Sahu <sauravsahu.12@live.com>
2017-10-14 15:32:20 +0530

    Update Greet.java

--- src/partb/Greet.java
+++ src/partb/Greet.java
@@ -10,11 +10,13 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet(urlPatterns = "/greet")
 public class Greet extends HttpServlet
 {
+	private static final long serialVersionUID = 4426880007296241022L;
+
 	public void service(HttpServletRequest req, HttpServletResponse res) throws IOException
 	{
-		String name = req.getParameter("userName").trim();
-		String responseText = name.compareTo("") == 0 ?
-				"Who are you?" : String.format("Hello there, %s..", name);
+		String name = req.getParameter("userName");
+		String responseText = name == null || name.trim().compareTo("") == 0 ?
+				"Who are you?" : String.format("Hello there, %s..", name.trim());
 		res.getWriter().println(responseText);
 	}
 }
